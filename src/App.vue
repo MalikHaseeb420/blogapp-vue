5a3a8677707a2b1af3475785d3483caaa04981de
Muhammad Haseeb Malik <haseeb.nazeer@devsinc.com>
2022-11-03 22:58:28 +0500

    Initial commit
    - create new vue component
    - how to define data variables in view js
    - used interpolation for data
    - how to define method in vue components
    - how to bind any functino of specific event like button click

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>App</h1>
+  <HomePage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HomePage from './components/Home.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="data">
+    <h1>Personal Details</h1>
+    <p><strong>Name: </strong>{{ name }}</p>
+    <p><strong>Email: </strong>{{ email }}</p>
+    <p><strong>Phone: </strong>{{ phone }}</p>
+    <button v-on:click="test()">Click me</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      name: 'Muhammad Haseeb Malik',
+      email: 'haseeb.nazeer@devsinc.com',
+      phone: '03094022674'
+    }
+  },
+  methods: {
+    test() {
+      alert('I am clicked')
+    }
+  }
+}
+</script>
+<style>
+.data {
+  max-width: 40%;
+  margin: 30px auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+</style>
